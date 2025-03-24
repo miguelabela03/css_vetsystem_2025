49294e965cffd63b30b1c7d4d4057447cbea3804
Miguel Abela <abelamiguel@gmail.com>
2025-03-24 20:19:40 +0100

    Added the basic add-appointment component

---
+++ src/app/add-appointment/add-appointment.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-appointment.component.html',
+  styleUrl: './add-appointment.component.css'
+})
+
+export class AddAppointmentComponent {
+
+}
