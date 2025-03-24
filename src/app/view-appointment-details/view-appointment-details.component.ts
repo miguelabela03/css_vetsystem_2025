78c60c772af1f24b9b35ce68e16b2c4199604eca
Miguel Abela <abelamiguel@gmail.com>
2025-03-24 20:18:21 +0100

    Added the basic view-appointment-details component

---
+++ src/app/view-appointment-details/view-appointment-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-appointment-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-appointment-details.component.html',
+  styleUrl: './view-appointment-details.component.css'
+})
+
+export class ViewAppointmentDetailsComponent {
+
+}
