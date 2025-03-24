ec260259d313ca21b8808b8788ab6d13be7fbf45
Miguel Abela <abelamiguel@gmail.com>
2025-03-24 20:16:15 +0100

    Added the basic list-appointment component

---
+++ src/app/list-appointment/list-appointment.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-appointment.component.html',
+  styleUrl: './list-appointment.component.css'
+})
+
+export class ListAppointmentComponent {
+
+}
