9fb70e744aa0cfe8ebfd2ada2d4d1609f6f8fb74
Miguel Abela <abelamiguel@gmail.com>
2025-03-24 20:21:18 +0100

    Added the basic update-appointment component

---
+++ src/app/update-appointment/update-appointment.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-appointment.component.html',
+  styleUrl: './update-appointment.component.css'
+})
+
+export class UpdateAppointmentComponent {
+
+}
