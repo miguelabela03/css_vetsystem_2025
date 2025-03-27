c00470051b6f8b1f4b1b5a43949aa18771f3c177
Miguel Abela <abelamiguel@gmail.com>
2025-03-28 00:58:40 +0100

    Added basic export functionality to Export to Excel button

--- src/app/list-appointment/list-appointment.component.ts
+++ src/app/list-appointment/list-appointment.component.ts
@@ -4,6 +4,7 @@ import { Appointment } from '../dto/appointment.dto';
 import { AppointmentService } from '../services/appointment.service';
 import { ConvertToStatusPipe } from '../pipes/convert-to-status.pipe';
 import Swal from 'sweetalert2';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-list-appointment',
@@ -16,6 +17,8 @@ import Swal from 'sweetalert2';
 export class ListAppointmentComponent implements OnInit {
 
   appointments: Appointment[] = [];
+
+  excelFileName = 'Appointments.xlsx';
 
   constructor(private appointmentService: AppointmentService) {
 
@@ -54,4 +57,37 @@ export class ListAppointmentComponent implements OnInit {
       }
     });
   }
+
+  // https://pankaj-kumar.medium.com/how-to-export-data-to-excel-file-in-angular-application-3d88a11900f9
+  // This function only exports the data showing within the table
+  // exportToExcel(): void
+  // {
+  //   /* pass here the table id */
+  //   let element = document.getElementById('export-table');
+  //   const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+  //   /* generate workbook and add the worksheet */
+  //   const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+  //   /* save to file */  
+  //   XLSX.writeFile(wb, this.excelFileName);
+  // }
+
+  exportToExcel(): void {
+    // Fetching all data from the API
+    this.appointmentService.getAppointments().subscribe((appointments: Appointment[]) => {
+
+        // Convert data to excel worksheet
+        const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(appointments);
+
+        //Create a new workbook and append the worksheet
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, this.excelFileName);
+
+        // Save to file
+        XLSX.writeFile(wb, this.excelFileName);
+    })
+  };
+
 }
